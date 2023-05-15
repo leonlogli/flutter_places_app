8b629007b16ec5c12aea6237c0747e7744c2608e
Léon Logli <leonlogli@gmail.com>
2023-05-15 22:14:58 +0200

    Add locations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_places_app/utils.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    getDeviceLocation().then(print);
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
